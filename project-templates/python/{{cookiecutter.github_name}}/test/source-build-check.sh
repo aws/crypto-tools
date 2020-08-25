23f022d46860636fa1376747a42d49920b5aa4b3
mattsb42-aws <bullocm@amazon.com>
2020-08-25 15:42:22 -0700

    chore: fix source-build-check - The sourcedist file discovery mechanism returned a relative path rather than a bare filename. - Also add more output to track status

--- project-templates/python/{{cookiecutter.github_name}}/test/source-build-check.sh
+++ project-templates/python/{{cookiecutter.github_name}}/test/source-build-check.sh
@@ -3,20 +3,26 @@
 #
 # NOTE: Humans should not run this file directly. If you want to run this check, run "tox -e sourcebuildcheck".
 
-WORKINGDIR=$1
-DISTDIR=$2
+WORKINGDIR=${1}
+DISTDIR=${2}
+
+echo "Performing source build check"
+echo "Using working directory ${WORKINGDIR}"
+echo "Using dist directory ${DISTDIR}"
 
 echo "Locating the source build and copying it into the working directory."
 DISTFILE=`ls $DISTDIR/{{cookiecutter.pypi_name}}-*.tar.gz | tail -1`
-cp $DISTFILE $WORKINGDIR
-DISTFILE=`ls $WORKINGDIR/{{cookiecutter.pypi_name}}-*.tar.gz | tail -1`
+echo "Found source build at ${DISTFILE}"
+cp ${DISTFILE} ${WORKINGDIR}
 
 echo "Extracting the source build."
-cd $WORKINGDIR
-tar xzvf $DISTFILE
-rm $DISTFILE
-EXTRACTEDDIR=`ls | tail -1`
-cd $EXTRACTEDDIR
+cd ${WORKINGDIR}
+NEWDISTFILE=$(ls {{cookiecutter.pypi_name}}-*.tar.gz | tail -1)
+echo "Using distfile ${NEWDISTFILE}"
+tar xzvf ${NEWDISTFILE}
+rm ${NEWDISTFILE}
+EXTRACTEDDIR=$(ls | tail -1)
+cd ${EXTRACTEDDIR}
 
 echo "Installing requirements from extracted source build."
 pip install -r test/requirements.txt
